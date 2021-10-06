2674d2aac3346e6a4c6482a5cc690c696457a3f2
twokf <mhoanga1@gmail.com>
2021-10-06 12:50:33 +0700

    Add style.css file

---
+++ main.js
@@ -0,0 +1,26 @@
+function OnClick1() {
+    document.getElementById("csgo1").style.display = "block";
+    document.getElementById("csgo2").style.display = "block";
+}
+
+function OnClick2() {
+    document.getElementById("lol1").style.display = "block";
+    document.getElementById("lol2").style.display = "block";
+
+}
+
+function X1() {
+    document.getElementById("csgo1").style.display = "none";
+    document.getElementById("csgo2").style.display = "none";
+}
+
+function X2() {
+    document.getElementById("lol1").style.display = "none";
+    document.getElementById("lol2").style.display = "none";
+}
+
+function show_noitification() {
+    const noitification = new Notification("Double click to close video!", {
+        body: "Alo hoang"
+    });
+}
